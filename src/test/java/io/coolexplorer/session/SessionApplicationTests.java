02ff01faec444b3c285d04cd4a5642dcd060dfcb
seunkim <seunkim@ea.com>
2022-01-17 23:52:50 -0800

    [Fixed unit test issue]

--- src/test/java/io/coolexplorer/session/SessionApplicationTests.java
+++ src/test/java/io/coolexplorer/session/SessionApplicationTests.java
@@ -1,9 +1,11 @@
-package io.coolexplorer;
+package io.coolexplorer.session;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 import static io.swagger.v3.oas.integration.StringOpenApiConfigurationLoader.LOGGER;
 
